20571fc09ab547783f31207c72a704c592abe001
test5test55555555 <test5test55555555@gmail.com>
2025-03-21 15:08:42 +0000

    35

--- src/app/groups/[id]/page.js
+++ src/app/groups/[id]/page.js
@@ -177,6 +177,7 @@ export default function Group() {
 
             <div className="chat-c">
             {
+                
                 allMsg.filter((msg) => msg.msgKey == group.groupKey).map((msg, index) => 
                     msg.type == "text" ? (
                         msg.from1 == user.userNum ? (
