4809c52e6cfcb7f9b004262bcda16c7fd9c8c157
test5test55555555 <test5test55555555@gmail.com>
2025-04-29 14:01:43 +0000

    update 10

--- src/app/chats/page.js
+++ src/app/chats/page.js
@@ -28,6 +28,7 @@ export default function Chats() {
         <main>
             
             
+      
                 <title>Chats</title>
         
           <h1 style={{textAlign:"center", paddingTop:"20px", paddingBottom:"20px"}}>Your Chats</h1>
